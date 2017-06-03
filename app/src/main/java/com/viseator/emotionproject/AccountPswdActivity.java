f39c4fbba39ed5855f559323ac79b3ab766eddd2
viseator <viseator@gmail.com>
2017-06-04 06:17:54 +0800

    fix ui

--- app/src/main/java/com/viseator/emotionproject/AccountPswdActivity.java
+++ app/src/main/java/com/viseator/emotionproject/AccountPswdActivity.java
@@ -39,7 +39,7 @@ public class AccountPswdActivity extends BaseActivity {
 
     @Override
     protected void baseInit() {
-
+        setFullScreen();
     }
 
     @Override
--- app/src/main/java/com/viseator/emotionproject/SignInOutActivity.java
+++ app/src/main/java/com/viseator/emotionproject/SignInOutActivity.java
@@ -25,6 +25,7 @@ public class SignInOutActivity extends BaseActivity {
 
     @Override
     protected void baseInit() {
+        setFullScreen();
 
     }
 
